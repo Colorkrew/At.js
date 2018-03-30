c265a0f57b5f82ce30031551c45889b35419e416
mukoyamak-isao <mukoyamak@K0419-001-180228.local>
2018-03-30 16:09:51 +0900

    fix compile bug

--- dist/js/jquery.atwho.js
+++ dist/js/jquery.atwho.js
@@ -1,3 +1,24 @@
+/**
+ * at.js - 1.5.4
+ * Copyright (c) 2018 chord.luo <chord.luo@gmail.com>;
+ * Homepage: http://ichord.github.com/At.js
+ * License: MIT
+ */
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module unless amdModuleId is set
+    define(["jquery"], function (a0) {
+      return (factory(a0));
+    });
+  } else if (typeof exports === 'object') {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory(require("jquery"));
+  } else {
+    factory(jQuery);
+  }
+}(this, function ($) {
 var DEFAULT_CALLBACKS, KEY_CODE;
 
 KEY_CODE = {
@@ -609,6 +630,7 @@ TextareaController = (function(superClass) {
     content += suffix;
     text = "" + startStr + content + (source.slice(this.query['endPos'] || 0));
     self = this.app;
+    console.log('fsdalkfjksdlajfjlsdajflk')
     if (!$inputor.is(':focus')) {
       $inputor.on('focus', function() {
         $inputor.one('change', function() {
@@ -1208,3 +1230,5 @@ $.fn.atwho["default"] = {
 };
 
 $.fn.atwho.debug = false;
+
+}));
